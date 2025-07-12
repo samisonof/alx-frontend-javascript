e7f6b78319ea11cfbdd2e74790212472209b3bec
Samuel <samiberakih3@gmail.com>
2025-07-12 18:01:59 +0300

    commit task 1 in alx-frontend-javascript

---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,24 @@
+interface StudentConstructor {
+  new (firstName: string, lastName: string): StudentClassInterface;
+}
+
+interface StudentClassInterface {
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+class StudentClass implements StudentClassInterface {
+  constructor(private firstName: string, private lastName: string) {}
+
+  workOnHomework(): string {
+    return "Currently working";
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
+}
+
+const student = new StudentClass("John", "Doe");
+console.log(student.displayName());    
+console.log(student.workOnHomework()); 
